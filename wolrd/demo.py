fdc4fe19f9bd2206d8b527059b405f05d42281d5
licuess <licuess@gmail.com>
2020-06-07 19:04:59 +0800

    update

--- wolrd/demo.py
+++ wolrd/demo.py
@@ -3,4 +3,4 @@ from requests import request
 # info = request("GET", url="http://ip.42.pl/raw")
 # info = request("GET", url="http://myip.ipip.net")
 info = request("GET", url="http://ipinfo.io/ip")
-print([info.text])
+print(info.text)
