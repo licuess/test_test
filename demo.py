0617ab459f2f45659304f4a78421ce7fb279baa4
licuess <licuess@gmail.com>
2020-06-08 00:26:31 +0800

    update

--- demo.py
+++ demo.py
@@ -4,5 +4,5 @@ import re
 
 """获取公网IP地址"""
 urls = ["http://ip.42.pl/raw", "http://myip.ipip.net", "http://ipinfo.io/ip"]
-infos = [res.read().decode('utf-8') for res in map(urlopen, urls) if res.getcode() == 200]
-addrs = [re.findall(r'\d+.\d+.\d+.\d+.', info) for info in infos]
+addrs = [re.findall(r'\d+.\d+.\d+.\d+.', res.read().decode('utf-8'))
+         for res in map(urlopen, urls) if res.getcode() == 200]
