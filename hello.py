45677f4f9e8cf0fd5b110a55e70370ac124046a7
licuess <licuess@gmail.com>
2023-02-11 18:18:10 +0800

    upgrade

--- hello.py
+++ hello.py
@@ -2,12 +2,14 @@ import atexit
 import time
 from itertools import product
 
-list1 = range(1, 3)
-list2 = range(4, 6)
-list3 = range(7, 9)
 
-for item1, item2, item3 in product(list1, list2, list3):
-	print(item1 + item2 + item3)
+def range_func():
+	list1 = range(1, 3)
+	list2 = range(4, 6)
+	list3 = range(7, 9)
+
+	for item1, item2, item3 in product(list1, list2, list3):
+		print(item1 + item2 + item3)
 
 
 @atexit.register
@@ -23,8 +25,11 @@ def world():
 
 
 def main():
+	clean()
+	world()
 	print("hello world!")
+	rangefunc()
 
 
-if __name___ == "__main__":
+if __name__ == "__main__":
 	main()
